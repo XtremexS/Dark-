20bcc2e20b6aaac09f9d40c8dce0a6c8da5a048f
XtremexS <73639787+XtremexS@users.noreply.github.com>
2020-11-09 11:01:33 +0800

    Yolo

--- main.js
+++ main.js
@@ -6,21 +6,15 @@ const request = require('request');
 
 const prefix = '_';
 
-var servers = {};
-
-const ytdl = require("ytdl-core");
-
 const fs = require('fs');
 
 const client = new Discord.Client();
 
-
-
 client.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('/commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+    const command = require(`/commands/${file}`);
  
     client.commands.set(command.name, command);
 }
